5a379bce52c65e56684f634dd239ab69129df71b
Ali Yiğit Yılmaz <redkom.yigit@gmail.com>
2024-09-05 01:55:23 +0300

    grass spawn animation remake

--- Assets/Scripts/Tile.cs
+++ Assets/Scripts/Tile.cs
@@ -9,14 +9,29 @@ public class Tile : MonoBehaviour
     public float spawnChance = 0.1f;
     private WorldGenerator worldGenerator;
 
+    private Animator animator;
+
     void Start()
     {
         worldGenerator = FindObjectOfType<WorldGenerator>();
         spawnableObjects = worldGenerator.objects;
+        animator = GetComponent<Animator>();
+
+        // Rastgele bir zamanlayýcý baþlatýyoruz.
+        float randomDelay = Random.Range(0f, 1f); // 0-1 saniye arasýnda rastgele bir deðer seçiyoruz
+        Invoke(nameof(PlaySpawnAnimation), randomDelay);
 
         if (isCleansed && Random.value < spawnChance)
         {
             SpawnObject();
+        }
+    }
+
+    void PlaySpawnAnimation()
+    {
+        if (animator != null)
+        {
+            animator.Play("Spawn"); // Animasyonun ismini ayarlayýn
         }
     }
 
